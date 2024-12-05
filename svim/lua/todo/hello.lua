050b97ad436bc755a4a2864512400f6c9023b372
Your Name <you@example.com>
2024-12-05 20:26:39 +0100

    chore: Remove unused game and configuration files from repository

--- svim/lua/todo/hello.lua
+++
@@ -1,8 +0,0 @@
--- This file contains the hello function.
--- It defines a simple function to print "Hello, world!" to the console.
-
-local function hello()
-  print("Hello, world!")
-end
-
-return hello
