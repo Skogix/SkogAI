2e6dcac09f88738d5f0a6a72fa1c88c087422e21
Skogix <Skogix@users.noreply.github.com>
2025-02-20 19:48:54 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Skogix"
+original_name="skogai"
+original_urlname="SkogAI"
+original_description="Awesome skogai created by Skogix"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv skogai $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
